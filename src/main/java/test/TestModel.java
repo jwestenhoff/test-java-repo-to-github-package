f6c9256a884849a4a02668675acfe13d5be32f64
Jonas Westenhoff <jwestenhoff@web.de>
2023-04-02 20:03:51 +0200

    add test model

---
+++ src/main/java/test/TestModel.java
@@ -0,0 +1,13 @@
+package test;
+
+public class TestModel {
+    private String text = "";
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
